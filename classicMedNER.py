756cc9ef6687a139f9126e8596f33995b77b422e
Ugwuozor Collins <ugwuozorcollinsemezie@gmail.com>
2023-04-21 20:00:39 +0100

    Update classicMedNER.py

--- classicMedNER.py
+++ classicMedNER.py
@@ -175,7 +175,7 @@ def main():
     text_result =""
     if st.button("Predict"):
         text_result = read_text_message(message_input)
-    st.success('The output is ': emzy)
+    st.success('The output is {}'.format(emzy))
     st.download_button(
     label="Download data",
     data=emzy,
